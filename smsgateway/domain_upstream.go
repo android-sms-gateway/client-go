51d4a1462127c191fab0db5a71e7abf63f2658bf
Aleksandr Soloshenko <capcom2me@gmail.com>
2025-05-22 20:40:06 +0700

    [upstream] add `SettingsUpdated` push event

--- smsgateway/domain_upstream.go
+++ smsgateway/domain_upstream.go
@@ -11,6 +11,8 @@ const (
 	PushWebhooksUpdated PushEventType = "WebhooksUpdated"
 	// Messages export is requested.
 	PushMessagesExportRequested PushEventType = "MessagesExportRequested"
+	// Settings are updated.
+	PushSettingsUpdated PushEventType = "SettingsUpdated"
 )
 
 // A push notification.
@@ -18,7 +20,7 @@ type PushNotification struct {
 	// The token of the device that receives the notification.
 	Token string `json:"token" validate:"required" example:"PyDmBQZZXYmyxMwED8Fzy"`
 	// The type of event.
-	Event PushEventType `json:"event" validate:"omitempty,oneof=MessageEnqueued WebhooksUpdated MessagesExportRequested" default:"MessageEnqueued" example:"MessageEnqueued"`
+	Event PushEventType `json:"event" validate:"omitempty,oneof=MessageEnqueued WebhooksUpdated MessagesExportRequested SettingsUpdated" default:"MessageEnqueued" example:"MessageEnqueued"`
 	// The additional data associated with the event.
 	Data map[string]string `json:"data"`
 }
