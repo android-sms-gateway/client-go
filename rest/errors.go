9fb8816845b54b4b3e78f41636219d9d9a74f332
Aleksandr Soloshenko <capcom2me@gmail.com>
2025-06-03 07:50:07 +0700

    [rest] structure errors system

--- rest/errors.go
+++ rest/errors.go
@@ -1,7 +1,37 @@
 package rest
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 var (
 	ErrAPIError = errors.New("api error")
+	ErrClient   = fmt.Errorf("%w: client error", ErrAPIError)
+	ErrServer   = fmt.Errorf("%w: server error", ErrAPIError)
 )
+
+var (
+	ErrBadRequest = fmt.Errorf("%w: validation failed", ErrClient)
+	ErrConflict   = fmt.Errorf("%w: conflict", ErrClient)
+)
+
+func IsAPIError(err error) bool {
+	return errors.Is(err, ErrAPIError)
+}
+
+func IsClientError(err error) bool {
+	return errors.Is(err, ErrClient)
+}
+
+func IsServerError(err error) bool {
+	return errors.Is(err, ErrServer)
+}
+
+func IsConflict(err error) bool {
+	return errors.Is(err, ErrConflict)
+}
+
+func IsBadRequest(err error) bool {
+	return errors.Is(err, ErrBadRequest)
+}
