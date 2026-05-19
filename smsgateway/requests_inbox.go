0b1538cba88c2317482705984250c4a5cfe2b0ad
Aleksandr Soloshenko <capcom2me@gmail.com>
2026-05-19 07:59:59 +0700

    [client] separate messages export request from the inbox refresh

--- smsgateway/requests_inbox.go
+++ smsgateway/requests_inbox.go
@@ -4,7 +4,7 @@ import "time"
 
 // InboxRefreshRequest represents a request to export messages.
 type InboxRefreshRequest struct {
-	DeviceID        string                `json:"deviceId"                  validate:"required,max=21"                    example:"PyDmBQZZXYmyxMwED8Fzy"`                    // ID of the device to refresh messages for.
+	DeviceID        *string               `json:"deviceId,omitempty"        validate:"omitempty,max=21"                   example:"PyDmBQZZXYmyxMwED8Fzy"`                    // ID of the device to refresh messages for.
 	Since           time.Time             `json:"since"                     validate:"required,ltefield=Until"            example:"2024-01-01T00:00:00Z"  format:"date-time"` // Start of the time range to refresh.
 	Until           time.Time             `json:"until"                     validate:"required,gtefield=Since"            example:"2024-01-01T23:59:59Z"  format:"date-time"` // End of the time range to refresh.
 	MessageTypes    []IncomingMessageType `json:"messageTypes,omitempty"    validate:"omitempty,min=1,dive,oneof=SMS MMS"`                                                    // List of message types to refresh. By default, SMS messages are refreshed.
@@ -14,4 +14,8 @@ type InboxRefreshRequest struct {
 // MessagesExportRequest represents a request to export messages.
 //
 // Deprecated: use InboxRefreshRequest instead.
-type MessagesExportRequest = InboxRefreshRequest
+type MessagesExportRequest struct {
+	DeviceID string    `json:"deviceId" validate:"required,max=21"         example:"PyDmBQZZXYmyxMwED8Fzy"`                    // ID of the device to refresh messages for.
+	Since    time.Time `json:"since"    validate:"required,ltefield=Until" example:"2024-01-01T00:00:00Z"  format:"date-time"` // Start of the time range to refresh.
+	Until    time.Time `json:"until"    validate:"required,gtefield=Since" example:"2024-01-01T23:59:59Z"  format:"date-time"` // End of the time range to refresh.
+}
