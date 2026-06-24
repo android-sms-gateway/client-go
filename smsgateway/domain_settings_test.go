57c99dff530c87e5e7f8a15c086d130db140604a
Aleksandr Soloshenko <capcom2me@gmail.com>
2026-06-24 11:23:27 +0700

    [domain] add work hours settings

--- smsgateway/domain_settings_test.go
+++ smsgateway/domain_settings_test.go
@@ -33,6 +33,16 @@ func TestDeviceSettings_Validate(t *testing.T) {
 			},
 			wantErr: false,
 		},
+		{
+			name: "messages with validation error",
+			settings: smsgateway.DeviceSettings{
+				Messages: &smsgateway.SettingsMessages{
+					SendIntervalMin: ptr(5),
+					SendIntervalMax: ptr(1),
+				},
+			},
+			wantErr: true,
+		},
 	}
 
 	for _, tt := range tests {
@@ -78,7 +88,146 @@ func TestSettingsMessages_Validate(t *testing.T) {
 			},
 			wantErr: true,
 		},
-		// Add more test cases as needed
+		{
+			name: "work hours enabled without start",
+			messages: smsgateway.SettingsMessages{
+				WorkHoursEnabled: ptr(true),
+				WorkHoursStart:   nil,
+				WorkHoursEnd:     ptr("18:00"),
+			},
+			wantErr: true,
+		},
+		{
+			name: "work hours enabled without end",
+			messages: smsgateway.SettingsMessages{
+				WorkHoursEnabled: ptr(true),
+				WorkHoursStart:   ptr("09:00"),
+				WorkHoursEnd:     nil,
+			},
+			wantErr: true,
+		},
+		{
+			name: "work hours enabled invalid start format",
+			messages: smsgateway.SettingsMessages{
+				WorkHoursEnabled: ptr(true),
+				WorkHoursStart:   ptr("9:00"),
+				WorkHoursEnd:     ptr("18:00"),
+			},
+			wantErr: true,
+		},
+		{
+			name: "work hours enabled invalid end format",
+			messages: smsgateway.SettingsMessages{
+				WorkHoursEnabled: ptr(true),
+				WorkHoursStart:   ptr("09:00"),
+				WorkHoursEnd:     ptr("6:00 PM"),
+			},
+			wantErr: true,
+		},
+		{
+			name: "work hours enabled invalid hour value",
+			messages: smsgateway.SettingsMessages{
+				WorkHoursEnabled: ptr(true),
+				WorkHoursStart:   ptr("24:00"),
+				WorkHoursEnd:     ptr("18:00"),
+			},
+			wantErr: true,
+		},
+		{
+			name: "work hours enabled invalid minute value",
+			messages: smsgateway.SettingsMessages{
+				WorkHoursEnabled: ptr(true),
+				WorkHoursStart:   ptr("09:00"),
+				WorkHoursEnd:     ptr("18:60"),
+			},
+			wantErr: true,
+		},
+		{
+			name: "work hours enabled seconds are not allowed",
+			messages: smsgateway.SettingsMessages{
+				WorkHoursEnabled: ptr(true),
+				WorkHoursStart:   ptr("09:00"),
+				WorkHoursEnd:     ptr("18:00:00"),
+			},
+			wantErr: true,
+		},
+		{
+			name: "work hours enabled valid",
+			messages: smsgateway.SettingsMessages{
+				WorkHoursEnabled: ptr(true),
+				WorkHoursStart:   ptr("09:00"),
+				WorkHoursEnd:     ptr("18:00"),
+			},
+			wantErr: false,
+		},
+		{
+			name: "work hours enabled overnight window",
+			messages: smsgateway.SettingsMessages{
+				WorkHoursEnabled: ptr(true),
+				WorkHoursStart:   ptr("22:00"),
+				WorkHoursEnd:     ptr("06:00"),
+			},
+			wantErr: false,
+		},
+		{
+			name: "work hours disabled ignores start/end",
+			messages: smsgateway.SettingsMessages{
+				WorkHoursEnabled: ptr(false),
+				WorkHoursStart:   nil,
+				WorkHoursEnd:     nil,
+			},
+			wantErr: false,
+		},
+		{
+			name: "send interval min only",
+			messages: smsgateway.SettingsMessages{
+				SendIntervalMin: ptr(1),
+				SendIntervalMax: nil,
+			},
+			wantErr: false,
+		},
+		{
+			name: "send interval max only",
+			messages: smsgateway.SettingsMessages{
+				SendIntervalMin: nil,
+				SendIntervalMax: ptr(1),
+			},
+			wantErr: false,
+		},
+		{
+			name: "equal send intervals",
+			messages: smsgateway.SettingsMessages{
+				SendIntervalMin: ptr(5),
+				SendIntervalMax: ptr(5),
+			},
+			wantErr: false,
+		},
+		{
+			name: "valid send interval range",
+			messages: smsgateway.SettingsMessages{
+				SendIntervalMin: ptr(1),
+				SendIntervalMax: ptr(10),
+			},
+			wantErr: false,
+		},
+		{
+			name: "work hours not enabled (nil)",
+			messages: smsgateway.SettingsMessages{
+				WorkHoursEnabled: nil,
+				WorkHoursStart:   nil,
+				WorkHoursEnd:     nil,
+			},
+			wantErr: false,
+		},
+		{
+			name: "work hours not enabled but start set",
+			messages: smsgateway.SettingsMessages{
+				WorkHoursEnabled: nil,
+				WorkHoursStart:   ptr("09:00"),
+				WorkHoursEnd:     nil,
+			},
+			wantErr: false,
+		},
 	}
 
 	for _, tt := range tests {
@@ -90,7 +239,6 @@ func TestSettingsMessages_Validate(t *testing.T) {
 	}
 }
 
-// Helper function to create a pointer to an int.
-func ptr(i int) *int {
+func ptr[T any](i T) *T {
 	return &i
 }
